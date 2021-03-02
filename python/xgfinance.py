bb5f4f6e380b3216d3ea401707c499de6af02ebe
Neil Schroeder <schr1077@umn.edu>
2021-03-02 15:27:08 -0600

    added an ignore file, and created the control script for the package

---
+++ python/xgfinance.py
@@ -0,0 +1,33 @@
+"""
+Main control script
+
+This is the main control script for this xgboost-finance package.
+"""
+
+import click
+import pandas as pd
+import numpy as np
+
+import scraper
+
+@click.command()
+@click.option('-t','--ticker', type=str, default='', required=False)
+@click.option('--extract', default=False, is_flag=True, required=False)
+def xgfinance(ticker, extract):
+    """Run the xgboost finance package."""    
+
+    #do some option management (if necessary)
+
+    #run the data extraction scripts
+    if extract:
+        scraper.get_data()
+
+    #send everything over to the model builder and train
+
+    #run the validation
+
+try:
+    if __name__ == '__main__':
+        xgfinance()
+except:
+    print("[ERROR] something went wrong in main")
